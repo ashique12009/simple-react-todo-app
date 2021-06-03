d2ce18af77098a59ed3bb4a4719f99f057e53046
Ashique <ashique12009@gmail.com>
2021-06-03 18:44:04 +0600

    active class set in nav link

--- src/Header.js
+++ src/Header.js
@@ -6,20 +6,20 @@ function Header() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Todo List</a>
+          <Link to="/" className="navbar-brand">Todo App</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link to="/" className="nav-link active">Home</Link>
+                <Link to="/" className="nav-link" activeClassName="active">Home</Link>
               </li>
               <li className="nav-item">
-                <Link to="/about" className="nav-link">About</Link>
+                <Link to="/about" className="nav-link" activeClassName="active">About</Link>
               </li>
               <li className="nav-item">
-                <Link to="/todo" className="nav-link">Todo</Link>
+                <Link to="/todo" className="nav-link" activeClassName="active">Todo</Link>
               </li>
             </ul>
           </div>
